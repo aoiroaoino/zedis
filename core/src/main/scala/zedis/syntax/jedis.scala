6cf51beb4c209f91a01d2444254355639d4fba27
aoino <aoiro.aoino+github@gmail.com>
2015-11-09 22:32:06 +0900

    fix syntax bug and add execOpt method.

--- core/src/main/scala/zedis/syntax/jedis.scala
+++ core/src/main/scala/zedis/syntax/jedis.scala
@@ -1,18 +1,25 @@
 package zedis.syntax
 
-import scalaz.{Monad, \/, Catchable}
+import scalaz.\/
 import scalaz.syntax.catchable._
 import scalaz.effect.IO
 
 import redis.clients.jedis.Jedis
 
-import zedis.free.jedis._
+trait JedisCommandSyntax {
+  import zedis.free.jedis._
 
-object jedis {
-  implicit class JedisCommandOps[M[_]: Monad: Catchable, A](ma: JedisCommand[A]) {
-    def exec(session: Jedis): Throwable \/ A =
+  implicit class JedisCommandOps[A](ma: JedisCommand[A]) {
+
+    def exec(session: Jedis): Throwable \/ A = {
       runCommand[IO, A](ma, session).attempt.unsafePerformIO.flatMap{ n =>
         if (n == null) \/.left(new Exception("not found value")) else \/.right(n)
       }
+    }
+
+    def execOpt(session: Jedis): Option[A] =
+      runCommand[IO, A](ma, session).attempt.unsafePerformIO.fold(_ => None, a => Option(a)) // nullチェック含む
   }
 }
+
+object jedis extends JedisCommandSyntax
