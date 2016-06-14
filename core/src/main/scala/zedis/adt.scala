f0197dbab97b9c8de4fa0178b42b52c539d60c30
aoino <aoiro.aoino+github@gmail.com>
2016-06-14 20:58:45 +0900

    add key commands

--- core/src/main/scala/zedis/adt.scala
+++ core/src/main/scala/zedis/adt.scala
@@ -4,7 +4,9 @@ object adt {
 
   sealed trait RedisCommandADT[+A]
 
+  //
   // hashes
+  //
 
   case class HDEL(key: String, fields: Seq[String]) extends RedisCommandADT[Long]
 
@@ -28,7 +30,6 @@ object adt {
 
   // case class HSCAN
 
-  // todo: value: T
   case class HSET[T](key: String, field: String, value: T, _codec: Codec[T]) extends RedisCommandADT[Long]
 
   case class HSETNX[T](key: String, field: String, value: T, _codec: Codec[T]) extends RedisCommandADT[Long]
@@ -36,4 +37,53 @@ object adt {
   case class HSTRLEN(key: String, field: String) extends RedisCommandADT[Long]
 
   case class HVALS[T](key: String, _codec: Codec[T]) extends RedisCommandADT[Seq[T]]
+
+  //
+  // keys
+  //
+
+  case class DEL(keys: Seq[String]) extends RedisCommandADT[Long]
+
+  case class DUMP(key: String) extends RedisCommandADT[Array[Byte]]
+
+  case class EXISTS(key: String) extends RedisCommandADT[Boolean]
+
+  case class EXPIRE(key: String, seconds: Long) extends RedisCommandADT[Boolean]
+
+  case class EXPIREAT(key: String, unixTime: Long) extends RedisCommandADT[Boolean]
+
+  case class KEYS(pattern: String) extends RedisCommandADT[Set[String]]
+
+  // case class MIGRATE
+
+  case class MOVE(key: String, dbIndex: Int) extends RedisCommandADT[Boolean]
+
+  // case class OBJECT
+
+  case class PERSIST(key: String) extends RedisCommandADT[Boolean]
+
+  case class PEXPIRE(key: String, milliseconds: Long) extends RedisCommandADT[Long]
+
+  case class PEXPIREAT(key: String, millisecondsUnixTime: Long) extends RedisCommandADT[Long]
+
+  case class PTTL(key: String) extends RedisCommandADT[Long]
+
+  case object RANDOMKEY extends RedisCommandADT[String]
+
+  case class RENAME(oldKey: String, newKey: String) extends RedisCommandADT[String]
+
+  case class RENAMENX(oldKey: String, newKey: String) extends RedisCommandADT[Boolean]
+
+  case class RESTORE(key: String, milliseconds: Long, data: Array[Byte]) extends RedisCommandADT[String]
+
+  // case class SCAN
+
+  // case class SORT
+
+  case class TTL(key: String) extends RedisCommandADT[Long]
+
+  // type is reserved word ...
+  case class VALUETYPE(key: String) extends RedisCommandADT[String]
+
+  // case class WAIT
 }
